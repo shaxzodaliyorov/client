cfc2ca6f16abfe06f070c95f3422a0b8e3033639
shaxzod <shaxzodaliyorov0202@gmail.com>
2023-01-27 13:39:59 +0500

    cleant 2

--- src/API/Config.js
+++ src/API/Config.js
@@ -1,3 +1,3 @@
 import axios from "axios";
-export const API = axios.create({ baseURL: 'http://localhost:5000/api' })
+export const API = axios.create({ baseURL: 'https://shaxzod-server.onrender.com/api' })
 // https://shaxzod-portfolio.onrender.com/api
